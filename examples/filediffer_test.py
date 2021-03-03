75d4a3ed3c3304c2527178daef4c27d855267ec9
Michael Mintz <mdmintz@gmail.com>
2021-03-03 13:27:21 -0500

    Update a test

--- examples/filediffer_test.py
+++ examples/filediffer_test.py
@@ -9,8 +9,8 @@ from seleniumbase import BaseCase
 class MyTestClass(BaseCase):
 
     def test_developsense_filediffer(self):
-        self.open("https://www.developsense.com/exercises/fda/filediffer.html")
         self.demo_mode = True
+        self.open("https://www.developsense.com/exercises/fda/filediffer.html")
         dir_name = os.path.dirname(os.path.abspath(__file__))
         file_path_1 = os.path.join(dir_name, "calendar_test.py")
         file_path_2 = os.path.join(dir_name, "kanban_test.py")
@@ -21,6 +21,6 @@ class MyTestClass(BaseCase):
         self.click("input#clear2")
         self.get_element('input#fileInput2').send_keys(file_path_2)
         self.click("input#diff")
-        self.assert_text("Base Text", "#showDiff table thead")
-        self.assert_text("New Text", "#showDiff table thead")
+        self.assert_element('th:contains("Base Text")')
+        self.assert_element('th:contains("New Text")')
         self.assert_element("#showDiff table tbody")
