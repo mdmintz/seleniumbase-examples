d9aa63f860a5cb131609b1a1d985cb3de661fd10
Michael Mintz <mdmintz@gmail.com>
2022-07-08 18:09:50 -0400

    Add example tests

---
+++ examples/test_canvas.py
@@ -0,0 +1,38 @@
+from seleniumbase import BaseCase
+
+
+class CanvasTests(BaseCase):
+    def get_pixel_colors(self):
+        # Return the RGB colors of the canvas element's top left pixel
+        x = 0
+        y = 0
+        if self.browser == "safari":
+            x = 1
+            y = 1
+        color = self.execute_script(
+            "return document.querySelector('canvas').getContext('2d')"
+            ".getImageData(%s,%s,1,1).data;" % (x, y)
+        )
+        if self.is_chromium():
+            return [color[0], color[1], color[2]]
+        else:
+            return [color["0"], color["1"], color["2"]]
+
+    def test_canvas_click_from_center(self):
+        self.open("https://seleniumbase.io/other/canvas")
+        self.click_with_offset("canvas", 0, 0, center=True)
+        self.sleep(1)  # Not needed (Lets you see the alert pop up)
+        alert = self.switch_to_alert()
+        self.assert_equal(alert.text, "You clicked on the square!")
+        self.accept_alert()
+        self.sleep(1)  # Not needed (Lets you see the alert go away)
+
+    def test_click_with_offset(self):
+        self.open("https://seleniumbase.io/canvas/")
+        self.highlight("canvas")
+        rgb = self.get_pixel_colors()
+        self.assert_equal(rgb, [221, 242, 231])  # Looks greenish
+        self.click_with_offset("canvas", 500, 350)
+        self.highlight("canvas", loops=5)
+        rgb = self.get_pixel_colors()
+        self.assert_equal(rgb, [39, 42, 56])  # Blue by hamburger
---
+++ examples/test_drag_and_drop.py
@@ -0,0 +1,22 @@
+"""
+Testing Drag & Drop on different pages.
+"""
+from seleniumbase import BaseCase
+
+
+class DragAndDropTests(BaseCase):
+    def test_drag_and_drop(self):
+        self.open("https://seleniumbase.io/other/drag_and_drop")
+        self.assert_element_not_visible("#div1 img#drag1")
+        self.drag_and_drop("#drag1", "#div1")
+        self.assert_element("#div1 img#drag1")
+        self.sleep(0.8)
+
+    def test_w3schools_drag_and_drop(self):
+        self.open("https://seleniumbase.io/w3schools/drag_drop")
+        self.remove_elements("#tryitLeaderboard")
+        self.switch_to_frame("iframeResult")
+        self.assert_element_not_visible("#div1 img#drag1")
+        self.drag_and_drop("#drag1", "#div1")
+        self.assert_element("#div1 img#drag1")
+        self.sleep(0.8)
