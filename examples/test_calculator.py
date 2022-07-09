0b46d0309d5662e2e134d587ef0fe3c03e1f7c0e
Michael Mintz <mdmintz@gmail.com>
2022-07-08 20:17:20 -0400

    Update the calculator tests

--- examples/test_calculator.py
+++ examples/test_calculator.py
@@ -2,19 +2,9 @@ from seleniumbase import BaseCase
 
 
 class CalculatorTests(BaseCase):
-    def test_6_times_7_plus_12_equals_54(self):
-        self.open("seleniumbase.io/apps/calculator")
-        self.click('button[id="6"]')
-        self.click("button#multiply")
-        self.click('button[id="7"]')
-        self.click("button#add")
-        self.click('button[id="1"]')
-        self.click('button[id="2"]')
-        self.click("button#equal")
-        self.assert_exact_text("54", "input#output")
-
     def test_42_divided_by_7_equals_6(self):
         self.open("seleniumbase.io/apps/calculator")
+        self.click("#clear")
         self.click('button[id="4"]')
         self.click('button[id="2"]')
         self.click("button#divide")
@@ -24,6 +14,7 @@ class CalculatorTests(BaseCase):
 
     def test_35_minus_21_equals_14(self):
         self.open("seleniumbase.io/apps/calculator")
+        self.click("#clear")
         self.click('button[id="3"]')
         self.click('button[id="5"]')
         self.click("button#subtract")
@@ -31,3 +22,33 @@ class CalculatorTests(BaseCase):
         self.click('button[id="1"]')
         self.click("button#equal")
         self.assert_exact_text("14", "input#output")
+
+    def test_6_times_7_plus_12_equals_54(self):
+        self.open("seleniumbase.io/apps/calculator")
+        self.click("#clear")
+        self.click('button[id="6"]')
+        self.click("button#multiply")
+        self.click('button[id="7"]')
+        self.click("button#add")
+        self.click('button[id="1"]')
+        self.click('button[id="2"]')
+        self.click("button#equal")
+        self.assert_exact_text("54", "input#output")
+
+    def test_special_code(self):
+        self.open("seleniumbase.io/apps/calculator")
+        self.click("#clear")
+        self.click('button[id="("]')
+        self.click('button[id="0"]')
+        self.click("button#add")
+        self.click('button[id="0"]')
+        self.click("button#add")
+        self.click('button[id="0"]')
+        self.click("button#add")
+        self.click('button[id="0"]')
+        self.click('button[id=")"]')
+        self.click("button#equal")
+        self.highlight('img[alt="SeleniumBase"]')
+        self.click('a[href*="seleniumbase.io/apps/calculator"]')
+        self.click("#clear")
+        self.highlight('a[href*="seleniumbase.io/apps/calculator"]')
