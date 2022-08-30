72f935354aff17a79deb6cc7272d6ae65fa99426
Michael Mintz <mdmintz@gmail.com>
2022-08-30 16:06:36 -0400

    Update an example test

--- examples/test_docs_site.py
+++ examples/test_docs_site.py
@@ -3,21 +3,17 @@ from seleniumbase import BaseCase
 
 class DocsSiteTests(BaseCase):
     def test_docs(self):
-        self.open("https://seleniumbase.io/")
         self.demo_mode = True
         self.highlights = 1
         self.demo_sleep = 0.1
-        self.message_duration = 0.5
-        self.assert_text("SeleniumBase", "h1")
-        self.js_click('a[href="help_docs/features_list/"]')
-        self.assert_exact_text("Features List", "h1")
-        self.js_click('a[href="../../examples/ReadMe/"]')
+        self.message_duration = 0.3
+        self.open("https://seleniumbase.io/examples/ReadMe/")
         self.assert_exact_text("Running Example Tests", "h1")
         self.js_click('a[href="../../help_docs/customizing_test_runs/"]')
         self.assert_exact_text("Command Line Options", "h1")
         self.js_click('a[href="../../examples/example_logs/ReadMe/"]')
         self.assert_exact_text("Dashboard / Reports", "h1")
-        self.js_click('[href="../../../seleniumbase/console_scripts/ReadMe/"]')
+        self.js_click('a[href*="/seleniumbase/console_scripts/ReadMe/"]')
         self.assert_exact_text("Console Scripts", "h1")
         self.js_click('a[href="../../../help_docs/syntax_formats/"]')
         self.assert_exact_text("Syntax Formats", "h1")
