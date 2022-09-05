cc42135ac94c012d3a7667fe0588c7bdc0b95e57
Michael Mintz <mdmintz@gmail.com>
2022-09-05 09:22:13 -0400

    Update an example test

--- examples/test_docs_site.py
+++ examples/test_docs_site.py
@@ -13,8 +13,6 @@ class DocsSiteTests(BaseCase):
         self.assert_exact_text("Command Line Options", "h1")
         self.js_click('a[href="../../examples/example_logs/ReadMe/"]')
         self.assert_exact_text("Dashboard / Reports", "h1")
-        self.js_click('a[href*="/seleniumbase/console_scripts/ReadMe/"]')
-        self.assert_exact_text("Console Scripts", "h1")
         self.js_click('a[href="../../../help_docs/syntax_formats/"]')
         self.assert_exact_text("Syntax Formats", "h1")
         self.js_click('a[href="../recorder_mode/"]')
