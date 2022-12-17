c9d4a0fb65deed7f28f707009fa2a86828a99abc
Michael Mintz <mdmintz@gmail.com>
2022-12-17 01:30:53 -0500

    Refresh Python dependencies

--- setup.py
+++ setup.py
@@ -50,9 +50,9 @@ setup(
     ],
     python_requires='>=3.6',
     install_requires=[
-        'seleniumbase>=4.9.7',
+        'seleniumbase>=4.9.8',
         'sbvirtualdisplay>=1.1.1',
-        'pdbp>=1.2.5',
+        'pdbp>=1.2.6',
         'tabcompleter>=1.1.0',
         ],
     packages=[
