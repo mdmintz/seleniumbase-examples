6e9909fde554b84f754a2a717d0116d034762fa1
Michael Mintz <mdmintz@gmail.com>
2023-11-01 09:54:53 -0400

    Add a user-agent test for headless UC Mode

---
+++ examples/test_ua_with_uc.py
@@ -0,0 +1,14 @@
+# Test the user-agent with UC Mode (Verify "Headless" is NOT in it)
+from seleniumbase import BaseCase
+BaseCase.main(__name__, __file__, "--uc", "--headless")
+
+
+class HeadlessUCTests(BaseCase):
+    def test_verify_user_agent_of_headless_uc_mode(self):
+        if not self.undetectable or not self.headless:
+            self.get_new_driver(undetectable=True, headless=True)
+        self.open("data:,")
+        user_agent = self.get_user_agent()
+        print('\nUser Agent = "%s"' % user_agent)
+        print('\nUser Agent (lower) = "%s"' % user_agent.lower())
+        self.assert_true("headless" not in user_agent.lower())
