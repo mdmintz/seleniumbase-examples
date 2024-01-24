877154f64512eccab28314ce80f0e82317f1b095
Michael Mintz <mdmintz@gmail.com>
2024-01-24 09:03:28 -0500

    Update a test

--- examples/my_first_test.py
+++ examples/my_first_test.py
@@ -1,26 +1,28 @@
 from seleniumbase import BaseCase
+BaseCase.main(__name__, __file__)
 
 
 class MyTestClass(BaseCase):
-
-    def test_basic(self):
+    def test_swag_labs(self):
         self.open("https://www.saucedemo.com")
         self.type("#user-name", "standard_user")
         self.type("#password", "secret_sauce\n")
-        self.assert_element("#inventory_container")
-        self.assert_text("PRODUCTS", "span.title")
+        self.assert_element("div.inventory_list")
+        self.assert_exact_text("Products", "span.title")
         self.click('button[name*="backpack"]')
         self.click("#shopping_cart_container a")
-        self.assert_text("YOUR CART", "span.title")
+        self.assert_exact_text("Your Cart", "span.title")
         self.assert_text("Backpack", "div.cart_item")
         self.click("button#checkout")
         self.type("#first-name", "SeleniumBase")
         self.type("#last-name", "Automation")
         self.type("#postal-code", "77123")
         self.click("input#continue")
-        self.assert_text("CHECKOUT: OVERVIEW")
+        self.assert_text("Checkout: Overview")
         self.assert_text("Backpack", "div.cart_item")
+        self.assert_text("29.99", "div.inventory_item_price")
         self.click("button#finish")
-        self.assert_exact_text("THANK YOU FOR YOUR ORDER", "h2")
+        self.assert_exact_text("Thank you for your order!", "h2")
         self.assert_element('img[alt="Pony Express"]')
         self.js_click("a#logout_sidebar_link")
+        self.assert_element("div#login_button_container")
