193ea7944aa61508dbae4b1d269851505e0486e2
Michael Mintz <mdmintz@gmail.com>
2024-10-23 18:09:21 -0400

    Update a test

--- examples/test_ua_with_uc.py
+++ examples/test_ua_with_uc.py
@@ -5,8 +5,6 @@ BaseCase.main(__name__, __file__, "--uc")
 
 class HeadlessUCTests(BaseCase):
     def test_verify_user_agent_of_headless_uc_mode(self):
-        if not self.undetectable or not self.headless:
-            self.get_new_driver(undetectable=True)
         self.open("data:,")
         user_agent = self.get_user_agent()
         print('\nUser Agent = "%s"' % user_agent)
