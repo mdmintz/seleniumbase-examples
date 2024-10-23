3c3b4a958091d5aebb1876dd4d0ccef8fd66ce6a
Michael Mintz <mdmintz@gmail.com>
2024-10-23 17:54:05 -0400

    Update a script

--- examples/test_ua_with_uc.py
+++ examples/test_ua_with_uc.py
@@ -1,12 +1,12 @@
 # Test the user-agent with UC Mode (Verify "Headless" is NOT in it)
 from seleniumbase import BaseCase
-BaseCase.main(__name__, __file__, "--uc", "--headless")
+BaseCase.main(__name__, __file__, "--uc")
 
 
 class HeadlessUCTests(BaseCase):
     def test_verify_user_agent_of_headless_uc_mode(self):
         if not self.undetectable or not self.headless:
-            self.get_new_driver(undetectable=True, headless=True)
+            self.get_new_driver(undetectable=True)
         self.open("data:,")
         user_agent = self.get_user_agent()
         print('\nUser Agent = "%s"' % user_agent)
