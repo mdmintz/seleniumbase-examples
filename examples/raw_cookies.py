c3c70bc7cce55e74c6393d21b10cafd400f22fc0
Michael Mintz <mdmintz@gmail.com>
2026-03-13 17:07:33 -0400

    Update a test

--- examples/raw_cookies.py
+++ examples/raw_cookies.py
@@ -1,7 +1,8 @@
 """A SeleniumBase test that loads cookies to bypass login."""
 from seleniumbase import SB
 
-with SB(test=True, uc=True, user_data_dir="my_user_dir") as sb:
+# Log in to Swag Labs and save cookies
+with SB(test=True) as sb:
     sb.open("https://www.saucedemo.com")
     sb.wait_for_element("div.login_logo")
     sb.type("#user-name", "standard_user")
@@ -10,8 +11,10 @@ with SB(test=True, uc=True, user_data_dir="my_user_dir") as sb:
     sb.highlight("div.inventory_list", loops=6)
     sb.save_cookies(name="cookies.txt")
 
-# Load previous saved cookies from udd to bypass login
-with SB(test=True, uc=True, user_data_dir="my_user_dir") as sb:
+# Load previously saved cookies to bypass login
+with SB(test=True) as sb:
+    sb.open("https://www.saucedemo.com")
+    sb.load_cookies(name="cookies.txt")
     sb.open("https://www.saucedemo.com/inventory.html")
     sb.highlight("div.inventory_list", loops=12)
     sb.save_screenshot_to_logs()
