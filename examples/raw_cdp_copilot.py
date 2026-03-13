d280c2aecd861c01af64c33946eb7d7290ab3302
Michael Mintz <mdmintz@gmail.com>
2026-03-13 13:49:33 -0400

    Update a test

--- examples/raw_cdp_copilot.py
+++ examples/raw_cdp_copilot.py
@@ -5,7 +5,7 @@ try:
     sb = sb_cdp.Chrome(url, locale="en", guest=True)
     textarea = "textarea#userInput"
     sb.wait_for_element(textarea)
-    sb.sleep(1.5)
+    sb.sleep(1.3)
     sb.click_if_visible('[aria-label="Dismiss"]')
     sb.sleep(0.5)
     sb.click('button[data-testid*="chat-mode-"]')
@@ -15,6 +15,15 @@ try:
     query = "How to start automating with SeleniumBase?"
     sb.press_keys(textarea, query)
     sb.sleep(1.1)
+    seen_text = sb.get_text(textarea)
+    if seen_text != query and seen_text in query:
+        # When CAPTCHA appears while typing text
+        sb.sleep(1.1)
+        sb.solve_captcha()
+        sb.sleep(2.2)
+        sb.type(textarea, "")
+        sb.press_keys(textarea, query)
+        sb.sleep(0.5)
     sb.click('button[data-testid="submit-button"]')
     sb.sleep(2.5)
     sb.solve_captcha()
